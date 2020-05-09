581bf1638227310b0faaf467574e3cf67902a2e8
Soul <vicente.rivera.r@usach.cl>
2020-05-09 19:02:37 -0400

    parte 1.2

---
+++ main.m
@@ -0,0 +1,5 @@
+f_1_2 = @(x) 6* exp(x + 18); 
+interval = [-10, 10];
+f_1_2_title = '6e^(^x^+^1^8^)';
+
+print_scales(f_1_2, f_1_2_title, interval)
---
+++ print_scales.m
@@ -0,0 +1,22 @@
+function [] = print_scales (f, f_title, interval)
+
+x_inf = interval(1);
+x_sup = interval(2);
+
+x = x_inf:0.05:x_sup;
+y = f(x);
+
+figure(1)
+plot(x, y, 'r -');
+title("Normal scale graph")
+xlabel('x')
+ylabel(f_title)
+
+figure(2)
+semilogy(x, y, 'r -')
+grid on
+title("Logarithmic scale graph")
+xlabel('x')
+ylabel(f_title)
+
+
